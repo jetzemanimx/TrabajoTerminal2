295d80853d90ce3e2e003c1c27857d57fe3bdb9c
Mucio Nava <mucio@MacBook-Pro-de-Mucio.local>
2017-03-01 16:53:11 -0600

    Este es mi primer commit y cree el modal

--- public/js/controllers/LoginCtrl.js
+++ public/js/controllers/LoginCtrl.js
@@ -1,12 +1,21 @@
-angular.module('LogCtrl', []).controller('LoginController', function($scope, $http, $rootScope, $location, $timeout){
-	$scope.ValidaLogin = function (){
-		$http.post('/api/user/login',{
+angular.module('LogCtrl', []).controller('LoginController', function($scope, $http, $rootScope, $location, $timeout,$mdDialog){
+	$scope.ValidaLogin = function (ev){
+		$http.post('http://192.168.1.104:8080/api/user/login',{
 			'rfc': $scope.RFC,
 			'password' : $scope.PWD
 		})
 			.success(function(data){
-				alert("Bienvenido");
-				//window.location.href = '/#/registerUser';
+				$mdDialog.show(
+		      $mdDialog.alert()
+		        .parent(angular.element(document.querySelector('#popupContainer')))
+		        .clickOutsideToClose(true)
+		        .title('Bienvenido')
+		        .textContent(data.personalData.Name +'  '+ data.personalData.lastName)
+		        .ariaLabel('Alert Dialog Demo')
+		        .ok('Aceptar')
+		        .targetEvent(ev)
+		    );
+				window.location.href = '/#/registerUser';
 			})
 			.error(function(error){
 				alert("Ops algo salio mal");
