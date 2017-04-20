17e3af0cafbbf1b97f310379f379c8c1c59710d1
Nancy sanchez <Nancy sanchez>
2017-04-20 15:34:14 -0500

    VB

--- public/js/controllers/vBCtrl.js
+++ public/js/controllers/vBCtrl.js
@@ -1,154 +1,102 @@
 angular.module('vBCtrl', []).controller('vBController', function($scope, $http, $rootScope, $location, $timeout, Message,Server, $route){
  	 
- 	var myDate = new Date();
+ 	 function DemoCtrl ($q, $timeout) {
+    var self = this;
+    var pendingSearch, cancelSearch = angular.noop;
+    var lastSearch;
 
- 	$scope.minDate = new Date(
- 			myDate.getFullYear(),
- 			myDate.getMonth(),
- 			myDate.getDate() + 1
- 		);
+    self.allContacts = loadContacts();
+    self.contacts = [self.allContacts[0]];
+    self.asyncContacts = [];
+    self.filterSelected = true;
 
- 	$scope.maxDate = new Date(
- 			myDate.getFullYear() + 20,
- 			myDate.getMonth(),
- 			myDate.getDate() + 1 
- 		);
+    self.querySearch = querySearch;
+    self.delayedQuerySearch = delayedQuerySearch;
 
- 	$scope.onlyWeekendsPredicate = function(date) {
-    var day = date.getDay();
-    return day === 1 || day === 2|| day === 3|| day === 4|| day === 5;
-  };
+    /**
+     * Search for contacts; use a random delay to simulate a remote call
+     */
+    function querySearch (criteria) {
+      return criteria ? self.allContacts.filter(createFilterFor(criteria)) : [];
+    }
 
+    /**
+     * Async search for contacts
+     * Also debounce the queries; since the md-contact-chips does not support this
+     */
+    function delayedQuerySearch(criteria) {
+      if ( !pendingSearch || !debounceSearch() )  {
+        cancelSearch();
 
-  $scope.hours = [
-          "07:00 ",
-          "07:30 ",
-          "08:00 ",
-          "08:30 ",
-          "09:00 ",
-          "09:30 ",
-          "10:00 ",
-          "10:30 ",
-					"11:00 ",
-					"11:30 ",
-					"12:00 ",
-					"12:30 ",
-					"13:00 ",
-					"13:30 ",
-					"14:00 ",
-					"14:30 ",
-					"15:00 ",
-					"15:30 ",
-					"16:00 ",
-					"16:30 ",
-					"17:00 ",
-					"17:30 ",
-					"18:00 ",
-					"18:30 ",
-					"19:00 ",
-					"19:30 ",
-					"20:00 ",
-					"20:30 ",
-					"21:00 ",
-					"21:30 "
-      ];
+        return pendingSearch = $q(function(resolve, reject) {
+          // Simulate async search... (after debouncing)
+          cancelSearch = reject;
+          $timeout(function() {
 
-      $scope.hoursf = [
-          "07:00 ",
-          "07:30 ",
-          "08:00 ",
-          "08:30 ",
-          "09:00 ",
-          "09:30 ",
-          "10:00 ",
-          "10:30 ",
-					"11:00 ",
-					"11:30 ",
-					"12:00 ",
-					"12:30 ",
-					"13:00 ",
-					"13:30 ",
-					"14:00 ",
-					"14:30 ",
-					"15:00 ",
-					"15:30 ",
-					"16:00 ",
-					"16:30 ",
-					"17:00 ",
-					"17:30 ",
-					"18:00 ",
-					"18:30 ",
-					"19:00 ",
-					"19:30 ",
-					"20:00 ",
-					"20:30 ",
-					"21:00 ",
-					"21:30 "
-      ];
+            resolve( self.querySearch(criteria) );
 
-      $scope.CalculaInicio = function (myDate) {
-
-      			var tmpHourInit='',
-						tmpMinuteInit='',
-						HourInit='';
-
-      			HourInit = $scope.hour;
-      			tmpHourInit = HourInit.substring(0,2);
-      			tmpMinuteInit = HourInit.substring(3,5);
-      			myDate.setHours(tmpHourInit);
-      			myDate.setMinutes(tmpMinuteInit);
-
-      			return myDate;
+            refreshDebounce();
+          }, Math.random() * 500, true)
+        });
       }
 
-      $scope.CalculaFinal = function (myDate1) {
-      		
-      			var tmpHourEnd='',
-						tmpMinuteEnd='',
-						HourEnd='';
+      return pendingSearch;
+    }
 
-      	    HourEnd = $scope.hourf;
-      			tmpHourEnd = HourEnd.substring(0,2);
-      			tmpMinuteEnd = HourEnd.substring(3,5);
-      			myDate1.setHours(tmpHourEnd);
-      			myDate1.setMinutes(tmpMinuteEnd);
+    function refreshDebounce() {
+      lastSearch = 0;
+      pendingSearch = null;
+      cancelSearch = angular.noop;
+    }
 
-      			return myDate1;      }
+    /**
+     * Debounce if querying faster than 300ms
+     */
+    function debounceSearch() {
+      var now = new Date().getMilliseconds();
+      lastSearch = lastSearch || now;
 
+      return ((now - lastSearch) < 300);
+    }
 
-      $scope.CrearPlantilla = function(){
+    /**
+     * Create filter function for a query string
+     */
+    function createFilterFor(query) {
+      var lowercaseQuery = angular.lowercase(query);
 
-      		var tmp1 = $scope.hour.substring(0,2) + $scope.hour.substring(3,5);
-      		var tmp2 = $scope.hourf.substring(0,2) + $scope.hourf.substring(3,5);
+      return function filterFn(contact) {
+        return (contact._lowername.indexOf(lowercaseQuery) != -1);
+      };
 
-      		if(!$scope.myDate || !$scope.myDate1){
-      			Message.Error('Ingresa una fecha');
-      		}
-      		else if($scope.hour == $scope.hourf){
-      			Message.Error('La hora no puede ser igual');
-      		}
-      		else if (tmp1 > tmp2){
-      			Message.Error("La hora de inicio no puede ser mayor a la hora final");
-      		}
-      		else if ($scope.myDate.getDate() != $scope.myDate1.getDate()){
-      			Message.Error('La fecha de votación tiene que ser el mismo día');
-      		}
-      		else{
-      			Dateini = $scope.CalculaInicio($scope.myDate);
-      			DateCalendar1 = $scope.CalculaFinal($scope.myDate1);
+    }
 
-      			$http.post('http://'+Server.Ip+'/api/votingBallot/register',{
-				        'name' :$scope.nombre,
-				        'init' : Dateini.toISOString(),
-				        'end' : DateCalendar1.toISOString()
-				      })
-				    .success(function (data) {
-				      Message.Success("Plantilla creada exitosamente");
-				      $route.reload();
-				    })
-				    .error(function (error) {
-				      Message.Error("Ops! algo salio mal, intenta nuevente");
-				    });
-      		}
-      }
+    function loadContacts() {
+      var contacts = [
+        'Marina Augustine',
+        'Oddr Sarno',
+        'Nick Giannopoulos',
+        'Narayana Garner',
+        'Anita Gros',
+        'Megan Smith',
+        'Tsvetko Metzger',
+        'Hector Simek',
+        'Some-guy withalongalastaname'
+      ];
+
+      return contacts.map(function (c, index) {
+        var cParts = c.split(' ');
+        var email = cParts[0][0].toLowerCase() + '.' + cParts[1].toLowerCase() + '@example.com';
+        var hash = CryptoJS.MD5(email);
+
+        var contact = {
+          name: c,
+          email: email,
+          image: '//www.gravatar.com/avatar/' + hash + '?s=50&d=retro'
+        };
+        contact._lowername = contact.name.toLowerCase();
+        return contact;
+      });
+    }
+  }
 });
