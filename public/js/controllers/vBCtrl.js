b72cb5f1ddb8de90610a90c9a3bf03f0e4ca3a8e
Nancy sanchez <Nancy sanchez>
2017-05-03 14:12:05 -0500

    Nancy

--- public/js/controllers/vBCtrl.js
+++ public/js/controllers/vBCtrl.js
@@ -1,102 +1,53 @@
-angular.module('vBCtrl', []).controller('vBController', function($scope, $http, $rootScope, $location, $timeout, Message,Server, $route){
- 	 
- 	 function DemoCtrl ($q, $timeout) {
-    var self = this;
-    var pendingSearch, cancelSearch = angular.noop;
-    var lastSearch;
+angular.module('vBCtrl', []).controller('vBController', contactsChipController);
 
-    self.allContacts = loadContacts();
-    self.contacts = [self.allContacts[0]];
-    self.asyncContacts = [];
-    self.filterSelected = true;
+         function contactsChipController ($scope,$http,Server) {
+            var self = this;
+            self.querySearch = querySearch;
+            self.allContacts = loadContacts();
+            self.contacts = [self.allContacts[0]];
+            self.filterSelected = true;
+            
+            function querySearch (query) {
+               var results = query ?
+               self.allContacts.filter(createFilterFor(query)) : [];
+               return results;
+            }
 
-    self.querySearch = querySearch;
-    self.delayedQuerySearch = delayedQuerySearch;
-
-    /**
-     * Search for contacts; use a random delay to simulate a remote call
-     */
-    function querySearch (criteria) {
-      return criteria ? self.allContacts.filter(createFilterFor(criteria)) : [];
-    }
-
-    /**
-     * Async search for contacts
-     * Also debounce the queries; since the md-contact-chips does not support this
-     */
-    function delayedQuerySearch(criteria) {
-      if ( !pendingSearch || !debounceSearch() )  {
-        cancelSearch();
-
-        return pendingSearch = $q(function(resolve, reject) {
-          // Simulate async search... (after debouncing)
-          cancelSearch = reject;
-          $timeout(function() {
-
-            resolve( self.querySearch(criteria) );
-
-            refreshDebounce();
-          }, Math.random() * 500, true)
-        });
-      }
-
-      return pendingSearch;
-    }
-
-    function refreshDebounce() {
-      lastSearch = 0;
-      pendingSearch = null;
-      cancelSearch = angular.noop;
-    }
-
-    /**
-     * Debounce if querying faster than 300ms
-     */
-    function debounceSearch() {
-      var now = new Date().getMilliseconds();
-      lastSearch = lastSearch || now;
-
-      return ((now - lastSearch) < 300);
-    }
-
-    /**
-     * Create filter function for a query string
-     */
-    function createFilterFor(query) {
-      var lowercaseQuery = angular.lowercase(query);
-
-      return function filterFn(contact) {
-        return (contact._lowername.indexOf(lowercaseQuery) != -1);
-      };
-
-    }
-
-    function loadContacts() {
-      var contacts = [
-        'Marina Augustine',
-        'Oddr Sarno',
-        'Nick Giannopoulos',
-        'Narayana Garner',
-        'Anita Gros',
-        'Megan Smith',
-        'Tsvetko Metzger',
-        'Hector Simek',
-        'Some-guy withalongalastaname'
-      ];
-
-      return contacts.map(function (c, index) {
-        var cParts = c.split(' ');
-        var email = cParts[0][0].toLowerCase() + '.' + cParts[1].toLowerCase() + '@example.com';
-        var hash = CryptoJS.MD5(email);
-
-        var contact = {
-          name: c,
-          email: email,
-          image: '//www.gravatar.com/avatar/' + hash + '?s=50&d=retro'
-        };
-        contact._lowername = contact.name.toLowerCase();
-        return contact;
-      });
-    }
-  }
-});
+            function createFilterFor(query) {
+               var lowercaseQuery = angular.lowercase(query);
+               return function filterFn(contact) {
+                  return (contact._lowername.indexOf(lowercaseQuery) != -1);;
+               };
+            }
+            
+            function loadContacts() {
+              var result = $http.get('http://'+Server.Ip+'/api/candidates')
+              .success(function(data){
+                return data;
+              })
+              .error(function(error){
+                return false;
+              });
+              // var contacts = [
+              //     'Roberto Karlos',
+              //     'Bob Crestor',
+              //     'Nigel Rick',
+              //     'Narayana Garner',
+              //     'Anita Gros',
+              //     'Megan Smith',
+              //     'Tsvetko Metzger',
+              //     'Hector Simek',
+              //     'James Roody'
+              //  ];
+              //  return contacts.map(function (c, index) {
+              //        var cParts = c.split(' ');
+              //        var contact = {
+              //           name: c,
+              //           email: cParts[0][0].toLowerCase() + '.' + cParts[1].toLowerCase() + '@example.com',
+              //           image: 'http://lorempixel.com/50/50/people?' + index
+              //        };
+              //        contact._lowername = contact.name.toLowerCase();
+              //        return contact;
+              //  });
+            }
+         }                 
